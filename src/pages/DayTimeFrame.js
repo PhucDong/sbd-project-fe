429db08031afa86df89d4402bb0393b4aa3dd03e
Phuc <dongvuminhphuc@gmail.com>
2024-05-30 14:33:00 +0700

    Add refresh icon

--- src/pages/DayTimeFrame.js
+++ src/pages/DayTimeFrame.js
@@ -1,10 +1,11 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton } from "@mui/material";
 import MainHeadingLayout from "../layouts/MainHeadingLayout";
 import MainButtonGroupLayout from "../layouts/MainButtonGroupLayout";
 import { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import AddFormLayout from "../layouts/AddFormLayout";
 import AddDayTimeFrameForm from "../components/AddDayTimeFrameForm";
+import CachedIcon from "@mui/icons-material/Cached";
 
 function DayTimeFrame() {
   const [openAddForm, setOpenAddForm] = useState(false);
@@ -39,6 +40,12 @@ function DayTimeFrame() {
         >
           Add
         </Button>
+
+        <IconButton
+          sx={{ p: 0, "&:hover": { backgroundColor: "transparent" } }}
+        >
+          <CachedIcon sx={{ fontSize: "2.1rem", color: "primary.main" }} />
+        </IconButton>
 
         <AddFormLayout
           openAddForm={openAddForm}
