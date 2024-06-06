93eba5a9483b76b8bd4f09a79b77dffa6287b53a
Phuc <dongvuminhphuc@gmail.com>
2024-06-06 09:44:23 +0700

    Add custom styled add button

--- src/pages/DayTimeFrame.js
+++ src/pages/DayTimeFrame.js
@@ -5,6 +5,9 @@ import { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import AddFormLayout from "../layouts/AddFormLayout";
 import AddDayTimeFrameForm from "../components/AddDayTimeFrameForm";
+import {
+  CustomStyledAddButton,
+} from "../components/CustomStyledAddButton";
 
 function DayTimeFrame() {
   const [openAddForm, setOpenAddForm] = useState(false);
@@ -15,30 +18,12 @@ function DayTimeFrame() {
     <Box>
       <MainHeadingLayout>Day Time Frame Setting</MainHeadingLayout>
       <MainButtonGroupLayout>
-        <Button
-          startIcon={<AddIcon />}
-          sx={{
-            backgroundColor: "success.main",
-            px: "20px",
-            py: "8px",
-            textTransform: "capitalize",
-            fontSize: "1.1rem",
-            fontWeight: 600,
-            lineHeight: "100%",
-            color: "#fff",
-            borderRadius: "20px",
-            fontFamily: "Open Sans, sans-serif",
-            "& .MuiSvgIcon-root": {
-              fontSize: "1.4rem",
-            },
-            "&:hover": {
-              backgroundColor: "success.main",
-            },
-          }}
+        <CustomStyledAddButton
           onClick={handleOpenAddForm}
+          startIcon={<AddIcon />}
         >
           Add
-        </Button>
+        </CustomStyledAddButton>
 
         <AddFormLayout
           openAddForm={openAddForm}
--- src/pages/WeekTimeFrame.js
+++ src/pages/WeekTimeFrame.js
@@ -1,51 +1,23 @@
-import { Box, Button } from "@mui/material";
+import { Box } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import MainHeadingLayout from "../layouts/MainHeadingLayout";
 import MainButtonGroupLayout from "../layouts/MainButtonGroupLayout";
 import { useState } from "react";
 import AddFormLayout from "../layouts/AddFormLayout";
 import AddWeekTimeFrameForm from "../components/AddWeekTimeFrameForm";
+import { CustomStyledAddButton } from "../components/CustomStyledAddButton";
 
 function WeekTimeFrame() {
   const [openAddForm, setOpenAddForm] = useState(false);
-  const handleOpenAddForm = () => setOpenAddForm(true);
   const handleCloseAddForm = () => setOpenAddForm(false);
 
   return (
     <Box>
       <MainHeadingLayout>Week Time Frame Setting</MainHeadingLayout>
       <MainButtonGroupLayout>
-        <Button
-          startIcon={<AddIcon />}
-          sx={{
-            backgroundColor: "success.main",
-            px: "20px",
-            py: "8px",
-            textTransform: "capitalize",
-            fontSize: "1.1rem",
-            fontWeight: 600,
-            lineHeight: "100%",
-            color: "#fff",
-            borderRadius: "20px",
-            fontFamily: "Open Sans, sans-serif",
-            "& .MuiSvgIcon-root": {
-              fontSize: "1.4rem",
-            },
-            "&:hover": {
-              backgroundColor: "success.main",
-            },
-          }}
-          onClick={handleOpenAddForm}
-        >
+        <CustomStyledAddButton startIcon={<AddIcon />}>
           Add
-        </Button>
-
-        <AddFormLayout
-          openAddForm={openAddForm}
-          handleCloseAddForm={handleCloseAddForm}
-        >
-          <AddWeekTimeFrameForm handleCloseAddForm={handleCloseAddForm} />
-        </AddFormLayout>
+        </CustomStyledAddButton>
       </MainButtonGroupLayout>
     </Box>
   );
