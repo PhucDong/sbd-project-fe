095ebe6d7c1637b98c5366780af2238a52006986
Phuc <dongvuminhphuc@gmail.com>
2024-06-07 15:26:34 +0700

    Create form button component

---
+++ src/components/CustomStyledFormButton.js
@@ -0,0 +1,31 @@
+import { Button } from "@mui/material";
+
+function CustomStyledFormButton(props) {
+  return (
+    <Button
+      onClick={props.onClick}
+      sx={{
+        border: props.children === "Cancel" ? "1px solid #A9A9A9" : "none",
+        py: "8px",
+        px: "20px",
+        width: "116px",
+        backgroundColor:
+          props.children === "Cancel" ? "#F0F0F0" : "secondary.main",
+        color: props.children === "Cancel" ? "info.main" : "#fff",
+        fontWeight: 550,
+        textTransform: "capitalize",
+        borderRadius: "20px",
+        lineHeight: "100%",
+        fontSize: "1.2rem",
+        "&:hover": {
+          backgroundColor:
+            props.children === "Cancel" ? "#F0F0F0" : "secondary.main",
+        },
+      }}
+    >
+      {props.children}
+    </Button>
+  );
+}
+
+export default CustomStyledFormButton;
