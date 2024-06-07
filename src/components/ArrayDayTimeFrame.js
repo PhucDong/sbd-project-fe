83afa8fbfb44a896267fef0d507611b2d1508ba1
Phuc <dongvuminhphuc@gmail.com>
2024-06-07 15:19:21 +0700

    Add component to render list of day time frames

--- src/components/ArrayDayTimeFrame.js
+++ src/components/ArrayDayTimeFrame.js
@@ -1,70 +1,94 @@
-import React, { useEffect, useState } from "react";
 import CustomTimePicker from "./CustomTimePicker";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import dayjs from "dayjs";
 
 function ArrayDayTimeFrame(props) {
-  const { onChange, formData, setFormData } = props;
+  const {
+    formData,
+    setFormData,
+    dayTimeFrames,
+    setDayTimeFrames,
+    errors,
+    isDayTimeFrameDeleted,
+    setIsDayTimeFrameDeleted,
+  } = props;
 
-  const [dayTimeFrames, setDayTimeFrames] = useState([
-    { startingTime: null, endingTime: null },
-  ]);
-
-  const handleChangeDayTimeFrame = (data, index) => {
+  const handleChangeTimePicker = (data, timeIndex) => {
     if (data.startingTime.isValid() && data.endingTime.isValid()) {
-      setDayTimeFrames((prevItems) => {
-        const updatedDayTimeFrames = prevItems.map((dayTimeFrame, i) =>
-          i === index ? { ...data } : dayTimeFrame
-        );
-        return updatedDayTimeFrames;
+      const updatedDayTimeFrames = dayTimeFrames.map((dayTimeFrame, index) => {
+        if (timeIndex === index) {
+          return { dayTimeFrameIndex: timeIndex, ...data };
+        }
+        return { dayTimeFrameIndex: timeIndex, ...dayTimeFrame };
       });
+
+      setDayTimeFrames(updatedDayTimeFrames);
+      setFormData({ ...formData, dayTimeFrames: [...updatedDayTimeFrames] });
     }
   };
 
   const handleAddNewTimeFrame = () => {
     setDayTimeFrames((prevItems) => [
       ...prevItems,
-      { startingTime: null, endingTime: null },
+      { startingTime: dayjs(null), endingTime: dayjs(null) },
     ]);
+    setFormData({
+      ...formData,
+      dayTimeFrames: [
+        ...dayTimeFrames,
+        { startingTime: dayjs(null), endingTime: dayjs(null) },
+      ],
+    });
   };
 
-  useEffect(() => {
-    // onChange(dayTimeFrames);
-    setFormData({ ...formData, dayTimeFrames });
-  }, [onChange, dayTimeFrames, setFormData, formData]);
+  const handleDeleteDayTimeFrame = (dayTimeFrameId) => {
+    const filteredDayTimeFrames = dayTimeFrames.filter(
+      (dayTimeFrame, index) => index !== dayTimeFrameId
+    );
+    setIsDayTimeFrameDeleted(true);
+    setDayTimeFrames(filteredDayTimeFrames);
+  };
 
   return (
     <>
-      {dayTimeFrames.length > 0 &&
-        dayTimeFrames.map((dayTimeFrame, index) => (
-          <CustomTimePicker
-            key={index}
-            timeFrameNumber={index}
-            startTime={dayTimeFrame.startingTime}
-            endTime={dayTimeFrame.endingTime}
-            // errorStartTime={errors?.startingTime}
-            // errorEndTime={errors?.endingTime}
-            onChange={(data) => handleChangeDayTimeFrame(data, index)}
-          />
-        ))}
+      {dayTimeFrames?.length > 0
+        ? dayTimeFrames.map((dayTimeFrame, index) => (
+            <CustomTimePicker
+              key={index}
+              dayTimeFrameNumber={index}
+              startTime={dayTimeFrame.startingTime}
+              endTime={dayTimeFrame.endingTime}
+              onChange={(data) => handleChangeTimePicker(data, index)}
+              errorStartingTime={
+                errors?.[`dayTimeFrames[${index}].startingTime`]
+              }
+              errorEndingTime={errors?.[`dayTimeFrames[${index}].endingTime`]}
+              handleDeleteDayTimeFrame={handleDeleteDayTimeFrame}
+              isDayTimeFrameDeleted={isDayTimeFrameDeleted}
+              setIsDayTimeFrameDeleted={setIsDayTimeFrameDeleted}
+            />
+          ))
+        : ""}
 
-      <Button
-        onClick={handleAddNewTimeFrame}
-        sx={{
-          backgroundColor: "secondary.main",
-          textTransform: "capitalize",
-          borderRadius: "20px",
-          lineHeight: "100%",
-          py: "8px",
-          px: "20px",
-          mt: "12px",
-          fontSize: "1rem",
-          color: "#fff",
-          fontWeight: 550,
-          "&:hover": { backgroundColor: "secondary.main" },
-        }}
-      >
-        Add Time Frame
-      </Button>
+      <Box sx={{ mt: "12px", mx: "auto" }}>
+        <Button
+          onClick={handleAddNewTimeFrame}
+          sx={{
+            backgroundColor: "secondary.main",
+            textTransform: "capitalize",
+            borderRadius: "20px",
+            lineHeight: "100%",
+            py: "8px",
+            px: "20px",
+            fontSize: "1rem",
+            color: "#fff",
+            fontWeight: 550,
+            "&:hover": { backgroundColor: "secondary.main" },
+          }}
+        >
+          Add Time Frame
+        </Button>
+      </Box>
     </>
   );
 }
