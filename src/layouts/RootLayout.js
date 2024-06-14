24c39e5e2fa84f9f6c49b01ad8183e13f746d199
Phuc <dongvuminhphuc@gmail.com>
2024-06-14 13:56:55 +0700

    Adjust the width of the main content

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,8 +1,37 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import SideMenu from "../components/SideMenu";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+
+export const drawerWidth = 300;
+
+const CustomStyledMainContent = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "openSideMenu",
+})(({ theme, openSideMenu }) => ({
+  flexGrow: 1,
+  height: "100vh",
+  width: `calc(100% - ${drawerWidth}px)`,
+  padding: "40px 30px",
+
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.easeOut,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  marginLeft: 0,
+
+  ...(!openSideMenu && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: `calc(100% - ${drawerWidth - 130}px)`,
+  }),
+}));
 
 function RootLayout() {
+  const [openSideMenu, setOpenSideMenu] = useState(true);
+
   return (
     <Box
       sx={{
@@ -12,12 +41,10 @@ function RootLayout() {
         alignItems: "stretch",
       }}
     >
-      <SideMenu />
-      <Box
-        sx={{ height: "100vh", width: "100%", px: { xs: 3 }, py: { xs: 5 } }}
-      >
+      <SideMenu openSideMenu={openSideMenu} setOpenSideMenu={setOpenSideMenu} />
+      <CustomStyledMainContent openSideMenu={openSideMenu}>
         <Outlet />
-      </Box>
+      </CustomStyledMainContent>
     </Box>
   );
 }
