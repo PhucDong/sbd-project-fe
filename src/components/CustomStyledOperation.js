043cbc28ad0a3d5b84b5c80cb10c44e985292a2a
Phuc <dongvuminhphuc@gmail.com>
2024-06-14 13:49:25 +0700

    Create custom styles for operation buttons

---
+++ src/components/CustomStyledOperation.js
@@ -0,0 +1,35 @@
+import { IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const iconStyles = {
+  fontSize: "1.6rem",
+  mr: "4px",
+};
+
+function CustomStyledOperation(props) {
+  const { onClick, children } = props;
+
+  return (
+    <>
+      <IconButton
+        sx={{
+          fontSize: "1rem",
+          color: children === "Modify" ? "secondary.main" : "error.main",
+          p: 0,
+        }}
+        onClick={onClick}
+      >
+        {children === "Modify" ? (
+          <EditIcon sx={iconStyles} />
+        ) : (
+          <DeleteIcon sx={iconStyles} />
+        )}
+        {children}
+      </IconButton>
+      
+    </>
+  );
+}
+
+export default CustomStyledOperation;
