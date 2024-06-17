2cd02cea84c9fca5af026aa5d6d3ac5e15bc72fe
pdqthai <thai28042003@gmail.com>
2024-06-17 16:53:14 +0700

    change

--- src/components/AddWeekTimeFrameForm.js
+++ src/components/AddWeekTimeFrameForm.js
@@ -1,4 +1,33 @@
-import { Button, MenuItem, TextField, Typography } from "@mui/material";
+import CreateIcon from "@material-ui/icons/Create"
+import {
+  Box, Button, Snackbar, Table,
+  TableBody, TableCell, TableHead, TableRow
+} from "@material-ui/core";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import DoneIcon from "@material-ui/icons/Done";
+import ClearIcon from "@material-ui/icons/Clear";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+const useStyles = makeStyles({
+  table: {
+    border: "2px solid #222C34",
+    width: "800px",
+    height: "200px",
+  },
+  th: {
+    borderBottom: "1px solid black",
+  },
+  td: {
+    textAlign: "center",
+  },
+})
 
 const selectOptions = [
   {
@@ -20,6 +49,17 @@ const selectOptions = [
 ];
 
 function AddWeekTimeFrameForm({ handleCloseAddForm }) {
+  const classes = useStyles();
+  const [rows, setRows] = useState([
+    { SerialNum: 1, Name: "", Monday: "", Tuesday: "", Wednesday: "", Thursday:"", Friday: "", Saturday: ""},
+  ]);
+
+  // Initial states
+  const handleCloseAddForm = (onCloseAddForm, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+  }
   return (
     <>
       <Typography>Add Week Time Frame</Typography>
--- src/pages/WeekTimeFrame.js
+++ src/pages/WeekTimeFrame.js
@@ -2,21 +2,46 @@ import { Box } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import MainHeadingLayout from "../layouts/MainHeadingLayout";
 import MainButtonGroupLayout from "../layouts/MainButtonGroupLayout";
-import { useState } from "react";
-import AddFormLayout from "../layouts/AddFormLayout";
-import AddWeekTimeFrameForm from "../components/AddWeekTimeFrameForm";
+import React, { useState } from "react";
 import { CustomStyledAddButton } from "../components/CustomStyledAddButton";
+import CreateIcon from "@material-ui/icons/Create";
+import {
+    Box, Button, Snackbar, Table,
+    TableBody, TableCell, TableHead, TableRow
+} from "@material-ui/core";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import DoneIcon from "@material-ui/icons/Done";
+import ClearIcon from "@material-ui/icons/Clear";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+const useStyles = makeStyles({
+  table: {
+    border: "2px solid #222C34",
+    width: "800px",
+    height: "200px",
+  },
+  th: {
+    borderBottom: "1px solid black",
+  },
+  td: {
+    textAlign: "center",
+  },
+});
 
 function WeekTimeFrame() {
-  const [openAddForm, setOpenAddForm] = useState(false);
-  const handleCloseAddForm = () => setOpenAddForm(false);
-
   return (
     <Box>
-      <MainHeadingLayout>Week Time Frame Setting</MainHeadingLayout>
+      <MainHeadingLayout>Week Time Frame Settin</MainHeadingLayout>
       <MainButtonGroupLayout>
         <CustomStyledAddButton startIcon={<AddIcon />}>
-          Add
+          Not Add
         </CustomStyledAddButton>
       </MainButtonGroupLayout>
     </Box>
