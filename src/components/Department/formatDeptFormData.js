89cfe2fc10b53bbb5d3de9a91e4e64ca327ff40a
Phuc <dongvuminhphuc@gmail.com>
2024-07-22 13:40:55 +0700

    Add format for dept form data

---
+++ src/components/Department/formatDeptFormData.js
@@ -0,0 +1,16 @@
+export const formatDeptFormData = (formData) => {
+  const currentUpperDeptName = formData.upperDeptName
+    .split(" ")
+    .filter((word) => word.length > 0)
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1).toLowerCase();
+    });
+  const formattedUpperDeptName = currentUpperDeptName.join(" ");
+
+  const newFormData = {
+    ...formData,
+    upperDeptName: formattedUpperDeptName,
+  };
+
+  return newFormData;
+};
