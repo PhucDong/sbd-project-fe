16718e15dba5a8bb4446eff3312e75379de4e106
Phuc <dongvuminhphuc@gmail.com>
2024-07-31 16:41:58 +0700

    Add styles and logic for Order List

--- src/components/_share/CustomStyledOperation.js
+++ src/components/_share/CustomStyledOperation.js
@@ -1,10 +1,31 @@
 import { IconButton } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import BallotIcon from "@mui/icons-material/Ballot";
 
-const iconStyles = {
-  fontSize: "1.6rem",
-  mr: "4px",
+const getOperationIcon = (children) => {
+  const iconStyles = {
+    fontSize: "1.6rem",
+    mr: "4px",
+  };
+
+  if (children === "Order List") {
+    return <BallotIcon sx={iconStyles} />;
+  } else if (children === "Modify") {
+    return <EditIcon sx={iconStyles} />;
+  } else if (children === "Delete") {
+    return <DeleteIcon sx={iconStyles} />;
+  }
+};
+
+const getColor = (children) => {
+  if (children === "Order List") {
+    return "primary.main";
+  } else if (children === "Modify") {
+    return "secondary.main";
+  } else if (children === "Delete") {
+    return "error.main";
+  }
 };
 
 function CustomStyledOperation(props) {
@@ -15,16 +36,12 @@ function CustomStyledOperation(props) {
       <IconButton
         sx={{
           fontSize: "1rem",
-          color: children === "Modify" ? "secondary.main" : "error.main",
+          color: getColor(children),
           p: 0,
         }}
         onClick={onClick}
       >
-        {children === "Modify" ? (
-          <EditIcon sx={iconStyles} />
-        ) : (
-          <DeleteIcon sx={iconStyles} />
-        )}
+        {getOperationIcon(children)}
         {children}
       </IconButton>
     </>
